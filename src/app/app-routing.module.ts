0ee31cd6acd8f851c305ae88e2fd84227768b3a3
MPilecarte <mayara.pilecarte@gmail.com>
2022-09-29 22:33:07 -0300

    Feature: create route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FilmesComponent } from './components/lancamentos/filmes/filmes.component';
+import { HomeComponent } from './template/home/home.component';
+import { SeriesComponent } from './components/lancamentos/series/series.component';
+
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'lançamentos', component:SeriesComponent},
+  {path: 'lançamento/filmes', component:FilmesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
 import { HomeComponent } from './template/home/home.component';
+import { SeriesComponent } from './components/lancamentos/series/series.component';
+import { FilmesComponent } from './components/lancamentos/filmes/filmes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    SeriesComponent,
+    FilmesComponent
   ],
   imports: [
     BrowserModule,
